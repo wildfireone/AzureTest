4b12ffbeac88229a550b50f9f4dc1b307585fa54
John <wildfireone@gmail.com>
2016-03-01 21:58:19 +0000

    added adminer

---
+++ people.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Query string</title>
+</head>
+<body>
+
+<?php
+
+// The value of the variable name is found
+echo "<h1>Hello " . $_GET["name"] . "</h1>";
+
+?>
+
+</body>
+</html>
