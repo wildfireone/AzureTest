2dbca1377848e9f25b048926eb48d06bd64cc87d
John <wildfireone@gmail.com>
2016-03-02 10:44:25 +0000

    added adminer

--- week5/connection.php
+++ week5/connection.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
 define('DB_USERNAME', 'b99256e599d9fc');
 define('DB_PASSWORD', '64c23a04');
