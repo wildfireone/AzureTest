5e519feef51eee8b4d4cfe923f79ed340fcb955f
John <wildfireone@gmail.com>
2016-03-02 10:46:31 +0000

    added adminer

--- week5/login.php
+++ week5/login.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include("connection.php"); //Establishing connection with our database
 
 $error = ""; //Variable for storing our errors.
