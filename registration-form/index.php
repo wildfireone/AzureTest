821b98ebe6bd3b23701a30892b6eaa9a86705f97
John <wildfireone@gmail.com>
2016-03-14 12:50:48 +0000

    added adminer

---
+++ registration-form/index.php
@@ -0,0 +1,91 @@
+<?php
+
+	include("connection.php");
+
+	$msg = "";
+	if(isset($_POST["submit"]))
+	{
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+
+		$name = mysqli_real_escape_string($db, $name);
+		$email = mysqli_real_escape_string($db, $email);
+		$password = mysqli_real_escape_string($db, $password);
+		$password = md5($password);
+
+
+		$sql="SELECT email FROM users WHERE email='$email'";
+		$result=mysqli_query($db,$sql);
+		$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+		if(mysqli_num_rows($result) == 1)
+		{
+			$msg = "Sorry...This email already exists...";
+		}
+		else
+		{
+			$query = mysqli_query($db, "INSERT INTO users (name, email, password)VALUES ('$name', '$email', '$password')");
+			if($query)
+			{
+				$msg = "Thank You! you are now registered.";
+			}
+		}
+	}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registration Form</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<form method="post" action="">
+		<fieldset>
+		<legend>Registration Form</legend>
+			<table width="400" border="0" cellpadding="10" cellspacing="10">
+				<tr>
+					<td colspan="2" align="center" class="error"><?php echo $msg;?></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold">
+							<div align="right"><label for="name">Name</label></div>
+					</td>
+					<td>
+							<input name="name" type="text" class="input" size="25" required />
+					</td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold">
+							<div align="right">
+									<label for="email">Email</label>
+							</div>
+					</td>
+					<td>
+							<input name="email" type="email" class="input" size="25" required />
+					</td>
+				</tr>
+				<tr>
+					<td height="23" style="font-weight: bold">
+							<div align="right">
+								<label for="password">Password</label>
+							</div>
+					</td>
+					<td>
+							<input name="password" type="password" class="input" size="25" required />
+					</td>
+				</tr>
+				<tr>
+					<td height="23"></td>
+					<td>
+						<div align="right">
+				  		<input type="submit" name="submit" value="Register!" />
+						</div>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
