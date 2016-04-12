637b61be81371ad26bb5f008fbed3c35213d3894
John <wildfireone@gmail.com>
2016-04-12 20:27:35 +0100

    added adminer

--- PhotoComment/userphotos.php
+++ PhotoComment/userphotos.php
@@ -6,17 +6,17 @@ if(isset($_SESSION['username']))
 
     $sql="SELECT userID FROM users WHERE username='$name'";
     $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $row=mysqli_fetch_assoc($result);
     if(mysqli_num_rows($result) == 1)
     {
         $searchID = $row['userID'];
-        $searchSql="SELECT title,photoID FROM photos WHERE userID='$searchID'";
+        $searchSql="SELECT title, photoID FROM photos WHERE userID='$searchID'";
         $searchresult=mysqli_query($db,$searchSql);
 
         if(mysqli_num_rows($searchresult)>0){
             while($searchRow = mysqli_fetch_assoc($searchresult)){
                 $line = "<p><a href='photos.php?id=".$searchRow['photoID']."'>".$searchRow['title']."</a></p>";
-                $resultText += $line;
+                $resultText = $resultText.$line;
             }
         }
         else{
