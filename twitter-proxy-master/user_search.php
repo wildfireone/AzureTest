026c6a0c371955c5be92f8d9e25ed2b67e1eba40
John <wildfireone@gmail.com>
2016-04-25 18:41:43 +0100

    added adminer

--- twitter-proxy-master/user_search.php
+++ twitter-proxy-master/user_search.php
@@ -7,20 +7,16 @@ $oauth_access_token = '14812487-6rIZpOfDmMBGFybfd26Cpe86kGw64MLGaZLaimuFN';
 $oauth_access_token_secret = 'efrJVLugBwFeAVnWSx1Cx6Z8N8IJlSB3js7XjW4KUYiuc';
 $consumer_key = 'uXl8060AfB6TtVZOjUPbPqiuU';
 $consumer_secret = 'qWYtc0c6wskWp9OvP63LcFTxnGexfhCjLAfN7gKgcwW7zfcSdv';
-$user_id = '14812487';
-$screen_name = 'wildfireone';
+$count=5;
+$q='default';
+
 if(isset($_GET['count'])){
 	$count = $_GET['count'];
-}
-else{
-	$count = 5;
 }
 if(isset($_GET['name'])){
 	$q = $_GET['name'];
 }
-else{
-	$q = 5;
-}
+
 
 $twitter_url = 'users/search.json';
 $twitter_url .= '?q=' . $q;
@@ -32,9 +28,7 @@ $twitter_proxy = new TwitterProxy(
 	$oauth_access_token,			// 'Access token' on https://apps.twitter.com
 	$oauth_access_token_secret,		// 'Access token secret' on https://apps.twitter.com
 	$consumer_key,					// 'API key' on https://apps.twitter.com
-	$consumer_secret,				// 'API secret' on https://apps.twitter.com
-	$q,								// User id (http://gettwitterid.com/)
-	$count							// The number of tweets to pull out
+	$consumer_secret				// 'API secret' on https://apps.twitter.com
 );
 
 // Invoke the get method to retrieve results via a cURL request
