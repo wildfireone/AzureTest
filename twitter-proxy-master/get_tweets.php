676a8875cf3fb99b0c2139b27bed4dfdaccd7f09
John <wildfireone@gmail.com>
2016-04-25 18:08:11 +0100

    added adminer

--- twitter-proxy-master/get_tweets.php
+++ twitter-proxy-master/get_tweets.php
@@ -7,20 +7,13 @@ $oauth_access_token = '14812487-6rIZpOfDmMBGFybfd26Cpe86kGw64MLGaZLaimuFN';
 $oauth_access_token_secret = 'efrJVLugBwFeAVnWSx1Cx6Z8N8IJlSB3js7XjW4KUYiuc';
 $consumer_key = 'uXl8060AfB6TtVZOjUPbPqiuU';
 $consumer_secret = 'qWYtc0c6wskWp9OvP63LcFTxnGexfhCjLAfN7gKgcwW7zfcSdv';
-$myuser_id = '14812487';
-$myscreen_name = 'wildfireone';
+$user_id = '14812487';
+$screen_name = 'wildfireone';
 $count = 5;
 
-
-if(isset($_GET['screen_name'])){
-	$screen_name = $_GET['screen_name'];
-}
-if(isset($_GET['count'])){
-	$count = $_GET['count'];
-}
-
 $twitter_url = 'statuses/user_timeline.json';
-$twitter_url .= '?screen_name=' . $screen_name;
+$twitter_url .= '?user_id=' . $user_id;
+$twitter_url .= '&screen_name=' . $screen_name;
 $twitter_url .= '&count=' . $count;
 
 // Create a Twitter Proxy object from our twitter_proxy.php class
@@ -29,8 +22,8 @@ $twitter_proxy = new TwitterProxy(
 	$oauth_access_token_secret,		// 'Access token secret' on https://apps.twitter.com
 	$consumer_key,					// 'API key' on https://apps.twitter.com
 	$consumer_secret,				// 'API secret' on https://apps.twitter.com
-	$myuser_id,						// User id (http://gettwitterid.com/)
-	$myscreen_name,					// Twitter handle
+	$user_id,						// User id (http://gettwitterid.com/)
+	$screen_name,					// Twitter handle
 	$count							// The number of tweets to pull out
 );
 
