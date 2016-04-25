22455f38d16f4c347236889b3cc94368370637ed
John <wildfireone@gmail.com>
2016-04-25 18:23:53 +0100

    added adminer

--- twitter-proxy-master/users.php
+++ twitter-proxy-master/users.php
@@ -19,7 +19,7 @@ else{
 $twitter_url = 'users/search.json';
 $twitter_url .= '?q=' . $_GET['name'];
 $twitter_url .= '&count=' . $count;
-console.log($twitter_url);
+echo $twitter_url;
 
 // Create a Twitter Proxy object from our twitter_proxy.php class
 $twitter_proxy = new TwitterProxy(
