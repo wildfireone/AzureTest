05e0e4518606e91fcd35e5d509a2fef682f156b5
John <wildfireone@gmail.com>
2016-04-26 20:50:58 +0100

    added adminer

---
+++ theprox.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Isaacs
+ * Date: 26/04/2016
+ * Time: 20:45
+ */
+$url = $_GET['url'];
+echo $_SERVER['QUERY_STRING']
+//$json = file_get_contents($url);
+//echo $json;
