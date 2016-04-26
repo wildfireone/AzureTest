cadd559840bad7ff8f6f531d2e0c488299604015
John <wildfireone@gmail.com>
2016-04-26 21:13:36 +0100

    added adminer

--- theprox.php
+++ theprox.php
@@ -19,7 +19,7 @@ echo "\n";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $finalurl);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
