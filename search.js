6d6eaf469641db1fd194b4e1190d2b5a86d02479
john <wildfireone@gmail.com>
2017-03-31 10:40:01 +0100

    added youtube

--- search.js
+++ search.js
@@ -1,3 +1,13 @@
+/**
+* @Author: John Isaacs <john>
+* @Date:   29-Mar-172017
+* @Filename: search.js
+* @Last modified by:   john
+* @Last modified time: 31-Mar-172017
+*/
+
+
+
 // After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
   console.log("enabling search");
@@ -17,10 +27,12 @@ function search() {
 
     var htmlstring = "";
     var videos = response.result.items;
+    console.log(response);
     $('#search-container').append("<ul>");
     for(var i=0; i<5; i++){
       var title = videos[i].snippet.title;
       $('#search-container').append("<li>"+title+"</li>");
+
     }
     $('#search-container').append("</ul>");
   });
